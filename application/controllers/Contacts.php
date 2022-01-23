f2639addca1f95577f5dc360ebf905bdc59af541
Ayeshmantha Madara Wijegunathileke <ayesh99747@gmail.com>
2022-01-23 09:45:04 +0530

    Changed ContactList controller name to Contacts

--- application/controllers/Contacts.php
+++ application/controllers/Contacts.php
@@ -1,11 +1,17 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class ContactList extends CI_Controller
+class Contacts extends CI_Controller
 {
 	public function index()
 	{
 		$data['main_view'] = "contacts_page";
+		$this->load->view('main', $data);
+	}
+
+	public function addNewContact()
+	{
+		$data['main_view'] = "add_contact_form";
 		$this->load->view('main', $data);
 	}
 }
